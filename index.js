11096440e7851b9601b4b687e6d62916677fd351
Junior Farabet <junior@rtone.fr>
2015-10-13 16:48:58 +0200

    fix bunyan

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var opbeat = require('opbeat')({
 	appId: '8889773ced',
 	organizationId: 'd52dc54a33064ddd9ce3d8ec8538ac82',
 	secretToken: '985678b054f3ae41939af6ffada034e27b89b906',
-	logger: require('bunyan')({ level: 'info' }),
+	logger: require('bunyan')({ name: "Testo", level: 'info' }),
 });
 
 app.set('port', (process.env.PORT || 5000));
