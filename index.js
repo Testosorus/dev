29ee11c0f97042fd92fcb11ee90e55066f0bdf57
Junior Farabet <junior@rtone.fr>
2015-10-13 16:22:29 +0200

    add opbeat handling

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var opbeat = require('opbeat')({
+	appId: '8889773ced',
+	organizationId: 'd52dc54a33064ddd9ce3d8ec8538ac82',
+	secretToken: '985678b054f3ae41939af6ffada034e27b89b906',
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -8,6 +13,8 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+app.use(opbeat.middleware.express());
 
 app.get('/', function(request, response) {
   response.render('pages/index');
